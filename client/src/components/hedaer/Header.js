c26eb22b0ca33e74559cab8ea04a4f33da0b56f6
Salt Dev <saltdev@DEV132.local>
2022-07-08 02:18:48 +0200

    styles fixed, need refactor

--- client/src/components/hedaer/Header.js
+++ client/src/components/hedaer/Header.js
@@ -5,7 +5,12 @@ const Header = () => {
 
 return (
 <div>
-  <div className="header">Header</div>
+  <div className="header">
+    <div className='logo'>
+      <h3 className='fake'>FAKE</h3>
+       <img src='https://ia.media-imdb.com/images/M/MV5BOTc2MzQ4MjAwM15BMl5BcG5nXkFtZTgwMzY2MzQ2OTE@._V1_.png' alt='logo' /> 
+    </div>
+  </div>
 </div>
 );
 };
