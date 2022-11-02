194f128ba8a0477c7490189fc8b4afe5393bc40f
MuhammadUsmanMushtaq <usman820901@gmail.com>
2022-11-02 19:21:26 +0100

    add env file

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require ('express');
+const express = require('express');
 require('dotenv').config();
-const axios = require ('axios');
+const axios = require('axios');
 const app = express();
-const api = process.env.API_KEY;
+const api = process.env.REACT_APP_API_KEY;
 
 app.get('/movies/:query', (req, res) => {
-  axios.get(`https://www.omdbapi.com/?s=${req.params.query}&apikey=${api}`)
-  .then(response => {
-    res.json(response.data)
-  }).catch( _error => {
-    res.json({message: 'Error occured!'})
-  })
-})
-
-
+  axios
+    .get(`https://www.omdbapi.com/?s=${req.params.query}&apikey=${api}`)
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((_error) => {
+      res.json({ message: 'Error occured!' });
+    });
+});
 
 const port = process.env.PORT;
 
-app.listen (port, () => console.log(`Server is running on ${port}`));
+app.listen(port, () => console.log(`Server is running on ${port}`));
